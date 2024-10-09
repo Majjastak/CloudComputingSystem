539ea6d12df9fee6e4b21c5f4435bf3a273ad6ab
Majjastak <dorian.peltier777@gmail.com>
2024-10-09 15:41:07 +0200

    step in the complition of exercise 1

---
+++ api/server.mjs
@@ -0,0 +1,17 @@
+import Fastify from "fastify";
+
+export function createServer() {
+  const fastify = Fastify();
+
+  fastify.get("/", async function handler(request, reply) {
+    const { code } = request.params;
+    return reply.send("sdfghj");
+  });
+
+  fastify.get("/feature", async function handler(request, reply) {
+    const { code } = request.params;
+    return reply.send({ code, enabled: true });
+  });
+
+  return fastify;
+}
