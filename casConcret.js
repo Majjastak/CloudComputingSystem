e1e1579f1d6094d0126fc7ceba2288b4c93779f4
Majjastak <dorian.peltier777@gmail.com>
2024-11-05 15:49:57 +0100

    Restructuration du morceau de code dans casConcret.js

--- casConcret.js
+++ casConcret.js
@@ -1,25 +1,25 @@
     if (x === 0) {
-        // Add a new line to arbrestruct and containstruct
-        const newLineIndex = containstruct.length;
-        arbrestruct.push([]);
-        containstruct.push([]);
+        // Add a new line to arbrestruct and containerStruct
+        const newRowIndex = containerStruct.length;
+        treeStruct.push([]);
+        containerStruct.push([]);
 
         // Copy the elements from the previous line to the new line
-        for (let j = 0; j < containstruct[newLineIndex - 1].length; j++) {
-            arbrestruct[newLineIndex][j] = arbrestruct[newLineIndex - 1][j];
-            containstruct[newLineIndex][j] = containstruct[newLineIndex - 1][j];
+        for (let j = 0; j < containerStruct[newRowIndex - 1].length; j++) {
+            treeStruct[newRowIndex][j] = treeStruct[newRowIndex - 1][j];
+            containerStruct[newRowIndex][j] = containerStruct[newRowIndex - 1][j];
         }
 
         // Set the elements in the previous line to null
-        for (let j = 0; j < containstruct[newLineIndex - 1].length; j++) {
-            arbrestruct[newLineIndex - 1][j] = null;
-            containstruct[newLineIndex - 1][j] = null;
+        for (let j = 0; j < containerStruct[newRowIndex - 1].length; j++) {
+            treeStruct[newRowIndex - 1][j] = null;
+            containerStruct[newRowIndex - 1][j] = null;
         }
 
-        for (let i = 0; i < containstruct.length; i++) {
-            for (let j = 0; j < containstruct[i].length; j++) {
-                if (containstruct[i][j]) {
-                    const container = containstruct[i][j];
+        for (let i = 0; i < containerStruct.length; i++) {
+            for (let j = 0; j < containerStruct[i].length; j++) {
+                if (containerStruct[i][j]) {
+                    const container = containerStruct[i][j];
                     container.style.left = `${100 + j * 163.867}px`;
                     container.style.top = `${200 + i * 297.8}px`;
                 }
@@ -27,22 +27,22 @@
         }
     }
 
-    arbrestruct[0].splice(y, 0, newTree);
-    containstruct[0].splice(y, 0, container);
+    treeStruct[0].splice(y, 0, newTree);
+    containerStruct[0].splice(y, 0, container);
 
 
-    console.log(containstruct);
+    console.log(containerStruct);
     document.body.appendChild(container, parentElement);
     container.style.position = "absolute";
     container.style.left = `${100 + y * 163.867}px`;
     container.style.top = `${200}px`;
 
     // Loop to update the horizontal positions of other containers in the same row
-    for (let i = y + 1; i < containstruct[0].length; i++) {
-        const currContainer = containstruct[0][i];
-        if(currContainer){
-            currContainer.style.left = `${parseInt(currContainer.style.left) + 297.8}px`;
-        }
+    for (let i = y + 1; i < containerStruct[0].length; i++) {
+        const currContainer = containerStruct[0][i];
+        
+        currContainer ? currContainer.style.left = `${parseInt(currContainer.style.left) + 297.8}px` : "";
+        
     }
 
     newTree.button2.style.display = "none";
@@ -52,29 +52,31 @@
 
     } else if (position === "right") {
 
+        const hasFather = !!treeStruct[x][y] && treeStruct[x][y].pere !== -1;
 
-
-    if (arbrestruct[x][y] && arbrestruct[x][y].pere !== -1) {
-        newTree.pere = arbrestruct[x][y].pere;
+    if (hasFather) {
+        // Add a sibling to the right of the current tree who has a father
+        newTree.pere = treeStruct[x][y].pere;
         newTree.button1.style.display = "none";
-        for (let i = 0; i < arbrestruct.length; i++) {
-            for (let j = 0; j < arbrestruct[i].length; j++) {
-                if (arbrestruct[i][j] && arbrestruct[i][j].id === newTree.pere) {
-                    arbrestruct[i][j].childlist.push(newTree.id);
-                    console.log(arbrestruct[i][j].childlist);
-                }
+        for (const personRow in treeStruct) {
+            for (const person in personRow) {
+                person.id === newTree.pere ? person.childList.push(newTree.id) : "";
+                console.log(person.childList);
             }
         }
     }
 
-    if (containstruct[x][y+1]) {
-        // Shift elements in containstruct[x] from y+1 to the end
-        for (let i = containstruct[x].length - 1; i >= y+1; i--) {
-            containstruct[x][i+1] = containstruct[x][i];
-            containstruct[x][i+1].style.left = `${100 + (i+1) * 163.867}px`;
-            containstruct[x][i+1].style.top = `${200 + x * 297.8}px`;
+    const potentialNeighboor = !!containerStruct[x][y+1];
+
+    if (potentialNeighboor) { 
+        // If there is atleast one container to the right of the current container we need to 
+        //shift all containers one position to the right
+        for (let i = containerStruct[x].length - 1; i >= y+1; i--) {
+            containerStruct[x][i+1] = containerStruct[x][i];
+            containerStruct[x][i+1].style.left = `${100 + (i+1) * 163.867}px`;
+            containerStruct[x][i+1].style.top = `${200 + x * 297.8}px`;
         }
-        for (let i = arbrestruct[x].length - 1; i >= y+1; i--) {
-            arbrestruct[x][i+1] = arbrestruct[x][i];
+        for (let i = treeStruct[x].length - 1; i >= y+1; i--) {
+            treeStruct[x][i+1] = treeStruct[x][i];
         }
     }
